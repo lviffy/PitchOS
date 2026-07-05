d99c7d6b051e60c6143c070e83aaef3a2a289aaf
lviffy <contact.rohan.here@gmail.com>
2026-07-05 14:48:14 +0530

    removed nm

--- packages/shared-types/dist/auth.d.ts
+++
@@ -1,4 +0,0 @@
-import { DIDChallenge, DIDAuthResponse } from './index';
-export declare function generateChallenge(): DIDChallenge;
-export declare function verifySignature(publicKeyHex: string, signatureHex: string, messageText: string): Promise<boolean>;
-export declare function verifyAuthResponse(challenge: DIDChallenge, response: DIDAuthResponse, maxAgeMs?: number): Promise<boolean>;
